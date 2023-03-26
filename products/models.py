a9fae3624d1fc226525f79d1028e1865b8ba2eba
Umar23A <abduaziz.botirov@mail.ru>
2023-03-26 21:19:57 +0500

    add admin panel

--- products/models.py
+++ products/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 
 class Product1(models.Model):
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     text = models.TextField()
-    cost = models.CharField()
+    cost = models.CharField(max_length=50)
 
 
 class Product2(models.Model):
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     text = models.TextField()
-    cost = models.CharField()
+    cost = models.CharField(max_length=50)
 
 
 class Product3(models.Model):
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     text = models.TextField()
-    cost = models.CharField()
+    cost = models.CharField(max_length=50)
 
 
 class Product4(models.Model):
-    image = models.ImageField(upload_to='/media')
+    image = models.ImageField(upload_to='media/')
     text = models.TextField()
-    cost = models.CharField()
+    cost = models.CharField(max_length=50)
 
 
