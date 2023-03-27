bb4357940270d33232d4aa752ecfb36689103457
ismoil <ismoillmirabbosoff@gmail.com>
2023-03-27 16:03:36 +0500

    change: changed uploadto

--- products/models.py
+++ products/models.py
@@ -2,31 +2,31 @@ from django.db import models
 
 
 class Product1(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     text = models.TextField()
     cost = models.CharField(max_length=50)
 
 
 class Product2(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     text = models.TextField()
     cost = models.CharField(max_length=50)
 
 
 class Product3(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     text = models.TextField()
     cost = models.CharField(max_length=50)
 
 
 class Product4(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     text = models.TextField()
     cost = models.CharField(max_length=50)
 
 
 class ProductTop(models.Model):
-    image = models.ImageField(upload_to='media/')
+    image = models.ImageField()
     text = models.TextField()
     cost = models.CharField(max_length=50)
 
